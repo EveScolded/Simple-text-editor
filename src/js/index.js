abe4b35935b27c494d43d3482d0022e1311202ba
Ewelina Rugała <ewelina.rugala93@gmail.com>
2020-12-06 19:41:21 +0100

    finished v 1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,29 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const saveButton = document.querySelector(".buttons__content--save-js");
+
+const entryInput = document.querySelector(".textarea__content--js");
+
+saveButton.addEventListener("click", () => {
+  localStorage.setItem("entry", entryInput.value);
+});
+
+const loadButton = document.querySelector(".buttons__content--load-js");
+
+loadButton.addEventListener("click", () => {
+  const entry = localStorage.getItem("entry");
+
+  let result = "";
+
+  if (entry) {
+    result = entry;
+  }
+  entryInput.value = result;
+});
+
+const clearButton = document.querySelector(".buttons__content--clear-js");
+
+clearButton.addEventListener("click", () => {
+    entryInput.value = '';    
+})
