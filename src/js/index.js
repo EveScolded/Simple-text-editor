1c8c80723e79b46634423f258da804b11bf78ad0
Ewelina Rugała <ewelina.rugala93@gmail.com>
2020-12-12 12:26:31 +0100

    adding ediotor buttons

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import "../scss/main.scss";
 // import {registerSW} from './pwa.js';
 // registerSW();
 
-/* place your code below */
+/* zapipsywanie do local storage */
 
 const saveButton = document.querySelector(".buttons__content--save-js");
 
@@ -13,6 +13,9 @@ const entryInput = document.querySelector(".textarea__content--js");
 saveButton.addEventListener("click", () => {
   localStorage.setItem("entry", entryInput.value);
 });
+
+
+/* wyświetlanie treści zapisanej w local storage */
 
 const loadButton = document.querySelector(".buttons__content--load-js");
 
@@ -27,8 +30,70 @@ loadButton.addEventListener("click", () => {
   entryInput.value = result;
 });
 
+
+/* usuwanie treści z textarea */
+
 const clearButton = document.querySelector(".buttons__content--clear-js");
 
 clearButton.addEventListener("click", () => {
     entryInput.value = '';    
-})
+});
+
+
+/* font style */
+
+const btnFontSelect = document.querySelector(".editor__button--fontStyle-js");
+
+function fontSelect() {
+  const selector = document.getElementById("select");
+  const value = selector[selector.selectedIndex].value;
+  entryInput.style.fontFamily = value;
+}
+
+btnFontSelect.addEventListener("change", fontSelect);
+
+
+/* font size */
+
+const btnFontSize = document.querySelector(".editor__button--fontSize-js");
+
+function fontSize () {
+ const fontSizeInput = document.getElementById("FontSize");
+ const size = fontSizeInput.value;
+ console.log(size);
+ entryInput.style.fontSize = `${size}px`; 
+}
+btnFontSize.addEventListener("click", fontSize);
+
+
+/* pogrubienie */
+
+const btnBold = document.querySelector(".editor__button--bold-js");
+
+function fontBolder() {
+  entryInput.classList.toggle("editor__button--bold");
+}
+btnBold.addEventListener("click", fontBolder);
+
+
+/* podkreślenie */
+
+const btnUnderline = document.querySelector(".editor__button--underline-js");
+
+function fontUnderline() {
+  entryInput.classList.toggle("editor__button--underline");
+}
+btnUnderline.addEventListener("click", fontUnderline);
+
+
+/* kursywa */
+
+const btnItalic = document.querySelector(".editor__button--italic-js");
+
+function fontItalic() {
+  entryInput.classList.toggle("editor__button--italic");
+}
+btnItalic.addEventListener("click", fontItalic);
+
+
+
